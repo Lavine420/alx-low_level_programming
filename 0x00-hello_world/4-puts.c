4b8033401b4f9a65e48990f02b566529edba1eae
lavine <nundhlallavine@gmail.com>
2023-02-16 16:04:31 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
- *
- * Return : 0 (success)
-*/
+ * Return:Always 0(Success)
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+		return (0);
+
 }
