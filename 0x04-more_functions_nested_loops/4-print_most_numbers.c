89fe47dde66945723075f98d0eabe209dd14b835
lavine <nundhlallavine@gmail.com>
2023-02-24 07:58:31 -0800

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,24 +3,23 @@
 /**
  * print_most_numbers - Print the numbers from 0 to 9
  * description - Print the numbers excluding 2 and 4
- * Return: The numbers from 0 to 9
+ * return: The numbera since 0 up to 9
  */
 
 void print_most_numbers(void)
-
 {
 	int x = 0;
 
-	for (; x <= 0, x++)
+	for (; x <= 9; x++)
 	{
-	if (x == 2 || x == 4)
-	{
-	continue;
-	}
-	else
-	{
-	_putchar(x + '0');
-	}
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
 	}
 	_putchar('\n');
 }
