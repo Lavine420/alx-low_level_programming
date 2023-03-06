06b15a26a2dd977702788dd4f452e48da48bb541
lavine <nundhlallavine@gmail.com>
2023-03-06 13:30:46 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,8 +2,8 @@
 
 /**
  * print_chessboard - Entry point
- * @a: array
- * Return: Always 0 (success)
+ * @a: Array
+ * Return: Alwways 0 (Success)
  */
 void print_chessboard(char (*a)[8])
 {
@@ -12,7 +12,7 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (n = 0; n < 8; n++)
-				_putchar(a[i][n]);
-			_putchar('\n');
+			_putchar(a[i][n]);
+		_putchar('\n');
 	}
 }
